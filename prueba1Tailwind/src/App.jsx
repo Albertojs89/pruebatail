b5e2adc6fcd3eb46eb54fe14379d79977b69f8f9
Albertojs89 <156692647+Albertojs89@users.noreply.github.com>
2025-03-25 11:52:52 +0000

    prueba ok, no funciona la extension

---
+++ prueba1Tailwind/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h2 className='text-3xl font-bold underline'>Prueba Tailwind</h2>
+    </>
+  )
+}
+
+export default App
